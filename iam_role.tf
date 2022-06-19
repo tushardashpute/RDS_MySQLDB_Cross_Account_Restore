32b2edf6487d1e1e53959d12b2432595ed491a80
tushardashpute <74225291+tushardashpute@users.noreply.github.com>
2022-06-19 16:38:36 +0530

    Create iam_role.tf

---
+++ iam_role.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          "AWS": "arn:aws:iam::750285159662:root"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = var.tag
+  }
+}
+
+  resource "aws_iam_role_policy_attachment" "test-attach" {
+    for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonRDSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+   ]) 
+ 
+   role       = aws_iam_role.test_role.name
+   policy_arn = each.value
+}
