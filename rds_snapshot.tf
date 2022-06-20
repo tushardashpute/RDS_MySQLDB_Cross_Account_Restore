ae2a04fb50dedd1e2c5ed12364eba202f2fa4df6
tushardashpute <74225291+tushardashpute@users.noreply.github.com>
2022-06-20 09:26:49 +0530

    Update rds_snapshot.tf

--- rds_snapshot.tf
+++ rds_snapshot.tf
@@ -1,16 +1,13 @@
 resource "aws_db_snapshot" "test" {
-  db_instance_identifier = "sourcerds"
-  db_snapshot_identifier = "testsnapshot1234"
-}
-
-output "db_snapshot_arn" {
-  description = "String. Name of the snapshot which a manual snapshot with custom CMK to be created."
-  value       = aws_db_snapshot.test.db_snapshot_arn
+  db_instance_identifier = var.db_instance
+  db_snapshot_identifier = var.db_snapshot
 }
 
 resource "null_resource" "output-id" {
   provisioner "local-exec" {
       command = "sh -vx test.sh"
   }
-  depends_on = ["aws_db_snapshot.test"]
+  depends_on = [
+     aws_db_snapshot.test
+  ]
 }
