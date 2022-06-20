1a515ecce2af8ebfaa2aaf8ad09e59c0a071b0b3
tushardashpute <74225291+tushardashpute@users.noreply.github.com>
2022-06-20 09:28:43 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,17 @@
+!#/bin/sh
+
+# Script will copy DB snapshot of encrypted snapshot with CMK and then shared it with target account. 
+
 aws rds  copy-db-snapshot --source-db-snapshot-identifier `terraform output  -json | jq -r '.db_snapshot_arn.value'` --target-db-snapshot-identifier myTFSnap  --source-region us-east-2 --kms-key-id `terraform output  -json | jq -r '.cmk_arn.value'`
-aws rds modify-db-snapshot-attribute --db-snapshot-identifier myTFSnap --attribute-name restore --values-to-add  750285159662
+
+echo "Checking DB Snapshot Status."
+Status=`aws rds describe-db-snapshots --db-snapshot-identifier myTFSnap | jq -r '.DBSnapshots[].Status'`
+
+while [ ${Status} != "available" ]
+do
+	echo "DB Snapshot is not yet availble."
+	sleep 30
+	Status=`aws rds describe-db-snapshots --db-snapshot-identifier myTFSnap | jq -r '.DBSnapshots[].Status'`
+done
+
+aws rds modify-db-snapshot-attribute --db-snapshot-identifier myTFSnap --attribute-name restore --values-to-add `terraform output  -json | jq -r '.target_account_id.value'`
